6fca408e5900cb4133aea118eb54f8cd74a66bda
Saint-Yellow Huang Jionghui <saint-yellow@hotmail.com>
2023-05-19 02:02:15 +0800

    polybar: modify configuration

--- polybar/themes/tiny/launch.sh
+++ polybar/themes/tiny/launch.sh
@@ -4,7 +4,9 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do
+  sleep 1;
+done
 
 polybar -r bar &
 
