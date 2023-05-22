76027c13580a5dd277d60a2ea0c3034c669dc7aa
Saint-Yellow Huang Jionghui <saint-yellow@hotmail.com>
2023-05-22 16:22:37 +0800

    nvim: modify configuration of jdtls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,10 +5,12 @@ local java_config = function()
     local defatul_config = require("lspconfig.server_configurations.jdtls").default_config
     local jdtls_path = mason_packages_path .. "/jdtls"
     local lombok_path = jdtls_path .. "/lombok.jar"
+    local config_path = jdtls_path .. "/config_linux"
 
     defatul_config.cmd = {
         jdtls_path .. "/bin/jdtls",
-        "--jvm_args=-javaagent:" .. lombok_path,
+        "-configuration" .. config_path,
+        "--jvm_arg=-javaagent:" .. lombok_path,
     }
     lspconfig.jdtls.setup(defatul_config)
 end
