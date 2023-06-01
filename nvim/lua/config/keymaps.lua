53325db1974caf22641a9d20127184b65e64ceaa
Saint-Yellow Huang Jionghui <saint-yellow@hotmail.com>
2023-06-01 22:43:30 +0800

    nvim: add custom keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+keymap.set("i", "<C-h>", "<Left>", { desc = "Move cursor LEFT in Insert mode", noremap = true, silent = true, })
+keymap.set("i", "<C-j>", "<Down>", { desc = "Move cursor DOWN in Insert mode", noremap = true, silent = true, })
+keymap.set("i", "<C-k>", "<Up>", { desc = "Move cursor UP in Insert mode", noremap = true, silent = true, })
+keymap.set("i", "<C-l>", "<Right>", { desc = "Move cursor RIGHT in Insert mode", noremap = true, silent = true, })
+
