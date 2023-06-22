84f77d75386d01142758118e0ccd6e0f4b22da6c
Saint-Yellow Huang Jionghui <saint-yellow@hotmail.com>
2023-06-23 00:03:39 +0800

    nvim: add new keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,11 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local keymap = vim.keymap
+local util = require("util")
+local map = util.keymap
 
-keymap.set("n", "qa", ":qa", { desc = "Quit Neovim", noremap = true, silent = true })
+map("i", "<C-h>", "<Left>", { desc = "Move cursor LEFT in Insert mode", noremap = true, silent = true })
+map("i", "<C-j>", "<Down>", { desc = "Move cursor DOWN in Insert mode", noremap = true, silent = true })
+map("i", "<C-k>", "<Up>", { desc = "Move cursor UP in Insert mode", noremap = true, silent = true })
+map("i", "<C-l>", "<Right>", { desc = "Move cursor RIGHT in Insert mode", noremap = true, silent = true })
 
-keymap.set("i", "<C-h>", "<Left>", { desc = "Move cursor LEFT in Insert mode", noremap = true, silent = true })
-keymap.set("i", "<C-j>", "<Down>", { desc = "Move cursor DOWN in Insert mode", noremap = true, silent = true })
-keymap.set("i", "<C-k>", "<Up>", { desc = "Move cursor UP in Insert mode", noremap = true, silent = true })
-keymap.set("i", "<C-l>", "<Right>", { desc = "Move cursor RIGHT in Insert mode", noremap = true, silent = true })
+map("n", "<leader>x", "<cmd>bp<bar>sp<bar>bn<bar>bd<cr>",
+    { desc = "Close current buffer", noremap = true, silent = true })
---
+++ nvim/lua/util/init.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+function M.keymap(mode, lhs, rhs, opts)
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+return M
